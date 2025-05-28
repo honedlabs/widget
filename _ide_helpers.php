74972d6833560081471a9ac7f962cb3f9ad2c979
jdw5 <joshuadwallace1@gmail.com>
2025-05-28 00:06:33 +0000

    feat(core): formatting, moved implicit route binding from action to core

--- _ide_helpers.php
+++ _ide_helpers.php
@@ -6,5 +6,5 @@ namespace Illuminate\Contracts\Foundation {
      * @method bool widgetsAreCached() Determine if the widgets are cached.
      * @method string getCachedWidgetsPath() Get the path to the widgets cache file.
      */
-    interface Application {}
+    interface Application { }
 }
